f121840627eb128e3c0770116a8d62082dee7d96
frosteen <luispambid@gmail.com>
2023-12-16 15:05:43 +0800

    Update

---
+++ (MATLAB)_DESKTOP-APP_Differential Equations-03/DifferentialEquations.m
@@ -0,0 +1,222 @@
+function varargout = DifferentialEquations(varargin)
+%DIFFERENTIALEQUATIONS MATLAB code file for DifferentialEquations.fig
+%      DIFFERENTIALEQUATIONS, by itself, creates a new DIFFERENTIALEQUATIONS or raises the existing
+%      singleton*.
+%
+%      H = DIFFERENTIALEQUATIONS returns the handle to a new DIFFERENTIALEQUATIONS or the handle to
+%      the existing singleton*.
+%
+%      DIFFERENTIALEQUATIONS('Property','Value',...) creates a new DIFFERENTIALEQUATIONS using the
+%      given property value pairs. Unrecognized properties are passed via
+%      varargin to DifferentialEquations_OpeningFcn.  This calling syntax produces a
+%      warning when there is an existing singleton*.
+%
+%      DIFFERENTIALEQUATIONS('CALLBACK') and DIFFERENTIALEQUATIONS('CALLBACK',hObject,...) call the
+%      local function named CALLBACK in DIFFERENTIALEQUATIONS.M with the given input
+%      arguments.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help DifferentialEquations
+
+% Last Modified by GUIDE v2.5 28-Mar-2019 00:07:56
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @DifferentialEquations_OpeningFcn, ...
+                   'gui_OutputFcn',  @DifferentialEquations_OutputFcn, ...
+                   'gui_LayoutFcn',  [], ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+   gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before DifferentialEquations is made visible.
+function DifferentialEquations_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   unrecognized PropertyName/PropertyValue pairs from the
+%            command line (see VARARGIN)
+
+% Choose default command line output for DifferentialEquations
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes DifferentialEquations wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+ah = axes('unit','normalized','position',[0 0 1 1]);
+bg = imread('bg.jpg'); imagesc(bg);
+set(ah, 'handlevisibility', 'off', 'visible', 'off');
+uistack(ah, 'bottom');
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = DifferentialEquations_OutputFcn(hObject, eventdata, handles)
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+function doReset()
+%format long;
+clc;
+
+function setString(handles, text)
+set(findobj(0, 'tag', handles), 'String', text);
+
+function v = getVal(handles)
+v = str2double(get(findobj(0, 'tag', handles), 'String'));
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit3_Callback(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit3 as text
+%        str2double(get(hObject,'String')) returns contents of edit3 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit4_Callback(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit4 as text
+%        str2double(get(hObject,'String')) returns contents of edit4 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+setString('edit1', '');
+setString('edit2', '');
+setString('edit3', '');
+setString('edit4', '');
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+doReset();
+p0 = getVal('edit1');
+k = getVal('edit2');
+r = getVal('edit3');
+t = getVal('edit4');
+p = (p0*k)/(p0+(k-p0)*exp(-r*t));
+data = [];
+h = 1;
+for x = 0:h:20
+   data = [data,(p0*k)/(p0+(k-p0)*exp(-r*(x)))]; 
+end
+plot(0:h:20,  data);
+xlabel('t');
+ylabel('Y');
+setString('text17',sprintf('%.6f',p));
+
+
+
+% --- Executes on button press in pushbutton8.
+function pushbutton8_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton8 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+cla;
+xlabel('');
+ylabel('');
