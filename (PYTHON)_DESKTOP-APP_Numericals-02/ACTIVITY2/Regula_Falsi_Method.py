0486a5701944ce7521ce5604282274db17857ed8
frosteen <luispambid@gmail.com>
2023-12-16 15:08:40 +0800

    Update

---
+++ (PYTHON)_DESKTOP-APP_Numericals-02/ACTIVITY2/Regula_Falsi_Method.py
@@ -0,0 +1,91 @@
+from tabulate import tabulate
+
+x = [0,0,0,0,0,0]
+decimalPlace = 2
+numChoose = 0
+err = 0.01
+
+def RegulaCoeff():
+    a0 = float(input("Enter the coefficient for x5: "))
+    a1 = float(input("Enter the coefficient for x4: "))
+    a2 = float(input("Enter the coefficient for x3: "))
+    a3 = float(input("Enter the coefficient for x2: "))
+    a4 = float(input("Enter the coefficient for x1: "))
+    a5 = float(input("Enter the coefficient for x0: "))
+
+    x[0] = a0
+    x[1] = a1
+    x[2] = a2
+    x[3] = a3
+    x[4] = a4
+    x[5] = a5
+    Regula()
+
+def Regula():
+    x0 = float(input("Enter the intital value x0: "))
+    x1 = float(input("Enter the intital value x1: "))
+
+    a0 = x[0]
+    a1 = x[1]
+    a2 = x[2]
+    a3 = x[3]
+    a4 = x[4]
+    a5 = x[5]
+
+    fx0 = (a0*x0**5 + a1*x0**4 + a2*x0**3 + a3*x0**2 + a4*x0**1 + a5)
+    fx1 = (a0*x1**5 + a1*x1**4 + a2*x1**3 + a3*x1**2 + a4*x1**1 + a5)
+
+    if (fx0>0 and fx1>0) or (fx0<0 and fx1<0):
+        print("Error initial conditions")
+        Regula()
+    else:
+
+        x2 = (x0-(fx0*(x1-x0)/(fx1-fx0)))
+        fx2 = (a0*x2**5 + a1*x2**4 + a2*x2**3 + a3*x2**2 + a4*x2**1 + a5)
+
+        rows = []
+        rows.append([x0, x1, x2, fx0, fx1, fx2, "CONTINUE"])
+
+        error = 1
+        while error > err:
+            fx0 = (a0*x0**5 + a1*x0**4 + a2*x0**3 + a3*x0**2 + a4*x0**1 + a5)
+            fx1 = (a0*x1**5 + a1*x1**4 + a2*x1**3 + a3*x1**2 + a4*x1**1 + a5)
+
+            x2 = (x0-(fx0*(x1-x0)/(fx1-fx0)))
+            fx2 = (a0*x2**5 + a1*x2**4 + a2*x2**3 + a3*x2**2 + a4*x2**1 + a5)
+            prevx2 = x2
+            if ((fx2)>0):
+                x1 = x2
+                x0 = x0
+                fx0 = (a0*x0**5 + a1*x0**4 + a2*x0**3 + a3*x0**2 + a4*x0**1 + a5)
+                fx1 = (a0*x1**5 + a1*x1**4 + a2*x1**3 + a3*x1**2 + a4*x1**1 + a5)
+                x2 = (x0-(fx0*(x1-x0)/(fx1-fx0)))
+                fx2 = (a0*x2**5 + a1*x2**4 + a2*x2**3 + a3*x2**2 + a4*x2**1 + a5)
+                #error = round(abs(fx2))
+                
+            elif ((fx2)<0):
+                x0 = x2
+                x1 = x1
+                fx0 = (a0*x0**5 + a1*x0**4 + a2*x0**3 + a3*x0**2 + a4*x0**1 + a5)
+                fx1 = (a0*x1**5 + a1*x1**4 + a2*x1**3 + a3*x1**2 + a4*x1**1 + a5)
+                x2 = (x0-(fx0*(x1-x0)/(fx1-fx0)))
+                fx2 = (a0*x2**5 + a1*x2**4 + a2*x2**3 + a3*x2**2 + a4*x2**1 + a5)
+                #error = round(abs(fx2))
+            #error = (abs(prevx2-x2))
+            error = abs(prevx2 - x2)
+            if error > err:
+                rows.append([x0, x1, x2, fx0, fx1, fx2, "CONTINUE"])
+            elif error < err or error == err:
+                rows.append([x0, x1, x2, fx0, fx1, fx2, "TERMINATE"])
+
+        print()
+        print(tabulate(rows, headers=['x0', 'x1', "x2", "f(x0)", "f(x1)", "f(x2)", "REMARKS"], tablefmt='plain', floatfmt=".{}f".format(decimalPlace)))
+        print()
+        print("ROOT:",round(x2, decimalPlace))
+    
+def main():
+    print("Regula Falsi Method")
+    RegulaCoeff()
+    input()
+    
+main()
